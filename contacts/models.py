812ce0e2200d0351174ccd83c3dfad09631759d9
Kolawole <kola.lawal91@yahoo.com>
2018-08-05 13:38:40 +0100

    Update models.py

--- contacts/models.py
+++ contacts/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class contact(models.Model):
     name = models.CharField(max_length=100)
-    emailAddress = models.CharField(max_length=100, unique=True)
+    emailAddress = models.CharField(max_length=100)
     date_logged = models.DateTimeField('Date added')
 
     def __str__(self):
